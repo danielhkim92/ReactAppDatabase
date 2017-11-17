6b4339df831ced7ffc497be69517415b933242d3
danielhkim92 <danielhkim92@gmail.com>
2017-11-16 21:05:20 -0600

    im lost

--- db/migrations.sql
+++ db/migrations.sql
@@ -1,21 +1,13 @@
-CREATE DATABASE personalitytypes;
+CREATE DATABASE project;
 
-\c personalitytypes
+\c project
 
 CREATE TABLE types (
 	id SERIAL PRIMARY KEY,
 	title VARCHAR(10),
-	youtubelink1 VARCHAR(255),
-	youtubelink2 VARCHAR(255),
-	youtubelink3 VARCHAR(255),
-	intro VARCHAR(5000),
-	strengths VARCHAR(5000),
-	weaknesses VARCHAR(5000),
-	emotions VARCHAR(5000),
-	romance VARCHAR(5000),
-	friendships VARCHAR(5000),
-	parenthood VARCHAR(5000),
-	career VARCHAR(5000),
-	work VARCHAR(5000),
-	conclusion VARCHAR(5000)
+	youtubelink1 VARCHAR(200),
+	youtubelink2 VARCHAR(200),
+	youtubelink3 VARCHAR(200),
+	intro VARCHAR(2000),
+	conclusion VARCHAR(2000)
 );
